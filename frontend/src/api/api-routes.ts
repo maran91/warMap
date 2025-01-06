32b30daf3cd740f9550901f023f8449d288a5004
Hendrik Nõgene <h.nogene@adcash.com>
2025-01-07 01:56:12 +0200

    Delete all files

--- frontend/src/api/api-routes.ts
+++
@@ -1,4 +0,0 @@
-export const SIGNUP_ROUTE = "api/signup";
-export const LOGIN_ROUTE = "api/login";
-export const LOGOUT_ROUTE = "api/logout";
-export const DISCOVER_ROUTE = "api/resources";
--- frontend/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-    readonly VITE_APP_BASE_URL: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
